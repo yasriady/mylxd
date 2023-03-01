8c5551f4a78de1915a22ff5512d7a8143561a3e7
Ubuntu <ubuntu@srv-134.pekanbaru.go.id>
2023-03-01 06:48:17 +0000

    OK

---
+++ mylxd.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# arg1 is Remote_Nama
+# exit if no argment
+if [[ $# -eq 0 ]] ; then
+    echo 'Silahkan sertakan REMOTE_NAME:'
+    exit 0
+fi
+
+REMSRV=${1}
+REMOTEHOSTS=($(lxc list ${REMSRV}: -c n --format csv))
+
+for HOST in "${REMOTEHOSTS[@]}"
+do
+    #OLD=${REMSRV}:${HOST}
+    REMOTEHOST=${REMSRV}:${HOST}
+    LOCALHOST=${HOST}
+
+    echo "Remote :" ${REMOTEHOST}
+    echo "Local  :" ${LOCALHOST}
+    echo "-------------------------------"
+    # Sync
+    echo lxc stop ${LOCALHOST}
+    # lxc stop ${LOCALHOST}
+    echo lxc copy ${REMOTEHOST} ${LOCALHOST} --refresh
+    # lxc copy ${REMOTEHOST} ${LOCALHOST} --refresh
+    echo lxc start ${LOCALHOST}
+    # lxc start ${LOCALHOST}
+    echo "==============================="
+done
+
